cbc62614ead52b75b650eef84ebf821d3336cafa
Ahmed Abdel Samea Khalifa <amidos2002@hotmail.com>
2018-11-26 17:58:27 -0500

    Adding PDF

--- PacmanComp/pacmanAgents.py
+++ PacmanComp/pacmanAgents.py
@@ -17,61 +17,6 @@ from game import Agent
 import random
 import math
 
-class RandomAgent(Agent):
-    # Initialization Function: Called one time when the game starts
-    def registerInitialState(self, state):
-        return;
-
-    # GetAction Function: Called with every frame
-    def getAction(self, state):
-        # get all legal actions for pacman
-        actions = state.getLegalPacmanActions()
-        # returns random action from all the valide actions
-        return actions[random.randint(0,len(actions)-1)]
-
-class RandomSequenceAgent(Agent):
-    # Initialization Function: Called one time when the game starts
-    def registerInitialState(self, state):
-        self.actionList = [];
-        for i in range(0,10):
-            self.actionList.append(Directions.STOP);
-        return;
-
-    # GetAction Function: Called with every frame
-    def getAction(self, state):
-        # get all legal actions for pacman
-        possible = state.getAllPossibleActions();
-        for i in range(0,len(self.actionList)):
-            self.actionList[i] = possible[random.randint(0,len(possible)-1)];
-        tempState = state;
-        for i in range(0,len(self.actionList)):
-            if tempState.isWin() + tempState.isLose() == 0:
-                tempState = tempState.generatePacmanSuccessor(self.actionList[i]);
-            else:
-                break;
-        # returns random action from all the valide actions
-        return self.actionList[0];
-
-class GreedyAgent(Agent):
-    # Initialization Function: Called one time when the game starts
-    def registerInitialState(self, state):
-        return;
-
-    # GetAction Function: Called with every frame
-    def getAction(self, state):
-        # get all legal actions for pacman
-        legal = state.getLegalPacmanActions()
-        # get all the successor state for these actions
-        successors = [(state.generatePacmanSuccessor(action), action) for action in legal]
-        # evaluate the successor states using scoreEvaluation heuristic
-        scored = [(scoreEvaluation(state), action) for state, action in successors]
-        # get best choice
-        bestScore = max(scored)[0]
-        # get all actions that lead to the highest score
-        bestActions = [pair[1] for pair in scored if pair[0] == bestScore]
-        # return random action from the list of the best actions
-        return random.choice(bestActions)
-
 class CompetitionAgent(Agent):
     # Initialization Function: Called one time when the game starts
     def registerInitialState(self, state):
